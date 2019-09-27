fa080c15532ca7f34c9a37ce1860cf6c0c2d1592
Francis PEROT <francis.perot@elca.ch>
2019-09-27 13:27:31 +0200

    Fix import of required actions

--- src/main/java/io/cloudtrust/keycloak/export/ImportExportUtils.java
+++ src/main/java/io/cloudtrust/keycloak/export/ImportExportUtils.java
@@ -1,9 +1,18 @@
 package io.cloudtrust.keycloak.export;
 
 import io.cloudtrust.keycloak.export.dto.BetterRealmRepresentation;
-import org.keycloak.models.*;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.services.managers.RealmManager;
 import org.keycloak.services.resources.KeycloakApplication;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class ImportExportUtils {
     public static RealmModel importRealm(KeycloakSession session, KeycloakApplication keycloak, BetterRealmRepresentation rep) {
@@ -12,6 +21,29 @@ public class ImportExportUtils {
             realmManager.setContextPath(keycloak.getContextPath());
         }
 
-        return realmManager.importRealm(rep);
+        // Cache required actions information and remove it from user representations..
+        // Original version from RepresentationToModel first convert it to an enum
+        // then get the name of the enum value. This fails for customized required actions
+        Map<UserRepresentation, List<String>> mapUserToRequiredActions = new HashMap<>();
+        for (UserRepresentation user : rep.getUsers()) {
+            if (user.getRequiredActions() != null) {
+                mapUserToRequiredActions.put(user, user.getRequiredActions());
+                user.setRequiredActions(Collections.emptyList());
+            }
+        }
+
+        // Basic import
+        RealmModel realm = realmManager.importRealm(rep);
+
+        // Now set required actions
+        for (Entry<UserRepresentation, List<String>> entry : mapUserToRequiredActions.entrySet()) {
+            UserRepresentation userRep = entry.getKey();
+            UserModel user = session.userLocalStorage().getUserById(userRep.getId(), realm);
+            for (String requiredAction : entry.getValue()) {
+                user.addRequiredAction(requiredAction.toUpperCase());
+            }
+        }
+
+        return realm;
     }
 }
--- src/test/java/io/cloudtrust/keycloak/export/dto/BetterRealmRepresentationTest.java
+++ src/test/java/io/cloudtrust/keycloak/export/dto/BetterRealmRepresentationTest.java
@@ -13,9 +13,12 @@ public class BetterRealmRepresentationTest {
         try (InputStream is = BetterRealmRepresentationTest.class.getClassLoader().getResourceAsStream("exported-realm.json")) {
             BetterRealmRepresentation realm = new ObjectMapper().readValue(is, BetterRealmRepresentation.class);
             Assert.assertEquals("Cloudtrust", realm.getId());
+            Assert.assertTrue(realm.getRequiredActions().stream().anyMatch(ra -> "NOT_AN_ENUM_VALUE".equals(ra.getAlias())));
 
             BetterUserRepresentation user = (BetterUserRepresentation) realm.getUsers().get(0);
             Assert.assertEquals("uglykidjoe", user.getUsername());
+            Assert.assertEquals(2, user.getRequiredActions().size());
+            Assert.assertTrue(user.getRequiredActions().contains("NOT_AN_ENUM_VALUE"));
 
             BetterCredentialRepresentation cred = (BetterCredentialRepresentation) user.getCredentials().get(0);
             Assert.assertEquals("1b9748f0-b9ba-4aaa-874f-0f0a3652dcf4", cred.getId());
